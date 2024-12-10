5e397461ef79352e1c2ea45657301d3651e63fe2
lai2-24 <lai2-24@itismeucci.com>
2024-12-10 14:37:07 +0100

    implementazioni

--- src/main/java/com/example/MyThread.java
+++ src/main/java/com/example/MyThread.java
@@ -2,21 +2,25 @@ package com.example;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.Socket;
 
-public class MyThread extends Thread{
+public class MyThread extends Thread {
     private Socket s;
 
-    public MyThread(Socket s){
+    public MyThread(Socket s) {
         this.s = s;
     }
 
-    public void run(){
+    public void run() {
         try (BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()))) {
             DataOutputStream out = new DataOutputStream(s.getOutputStream());
 
+            String responseBody;
             String firstLine = in.readLine();
             System.out.println(firstLine);
             String[] request = firstLine.split(" ");
@@ -25,6 +29,10 @@ public class MyThread extends Thread{
             String resource = request[1];
             String version = request[2];
             String header;
+            File file;
+            InputStream input;
+            byte[] buf;
+            int n;
 
             do {
                 header = in.readLine();
@@ -33,15 +41,73 @@ public class MyThread extends Thread{
 
             System.out.println("Richiesta terminata");
 
-            String responseBody = "Ciao a tutti";
-            out.writeBytes("HTTP/1.1 200 OK\n");
-            out.writeBytes("Content-Type: text/plain\n");
-            out.writeBytes("Content-Lenght: " + responseBody.length() + "\n");
-            out.writeBytes("\n");
-            out.writeBytes(responseBody);
+            switch (resource) {
+                case "/":
+                
+                case "/index.html":
+
+                     file = new File("htdocs/index.html");
+                     input = new FileInputStream(file);
+
+                    out.writeBytes("HTTP/1.1 200 OK\n");
+                    out.writeBytes("Content-Length: " + file.length() + "\n");
+                    out.writeBytes("Content-Type: text/html\n");
+                    out.writeBytes("\n");
+                    buf = new byte[8192];
+                   
+                    while((n = input.read(buf)) != -1){
+                        out.write(buf, 0, n);
+                    }
+                    input.close();
+                    break;
+                
+                case "/file.txt":
+                 file = new File("htdocs/file.txt");
+                 input = new FileInputStream(file);
+
+                out.writeBytes("HTTP/1.1 200 OK\n");
+                out.writeBytes("Content-Length: " + file.length() + "\n");
+                out.writeBytes("Content-Type: text/plain\n");
+                out.writeBytes("\n");
+                buf = new byte[8192];
+                
+                while((n = input.read(buf)) != -1){
+                    out.write(buf, 0, n);
+                }
+                input.close();
+                break;
+                
+                default:
+                    out.writeBytes("HTTP/1.1 404 Not Found\n");
+                    out.writeBytes("Content-Length: 0"  + "\n");
+                    out.writeBytes("\n");
+
+              
+            }
+
+            
+
         } catch (IOException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
+    }
+
+    public String getConentType(File f){
+        String[] s = f.getName().split("\\.");
+        String ext = s[s.length - 1];
+        switch(ext){
+            case "html":
+            case "htm":
+                return "text/html";
+            case "pnj":
+                return "image/png";
+            case "css":
+                return "text/css";
+            default:
+                return "";
+        }
+        
+
     }
 }
